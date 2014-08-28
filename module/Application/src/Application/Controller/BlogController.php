7eced277e6faaffc9681da7a2816c72c30fa1412
tomasgeci <tomas.geci@gmail.com>
2014-08-28 13:03:51 +0200

    routes
    Added routes and some view examples based on video:
    
    Zend Framework 2.1 tutorial 3: Routers

---
+++ module/Application/src/Application/Controller/BlogController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+* Zend Framework (http://framework.zend.com/)
+*
+* @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+* @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+* @license   http://framework.zend.com/license/new-bsd New BSD License
+*/
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class BlogController extends AbstractActionController // -tge- this is blog controller
+{
+    // -tge- index action for more posts
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+
+    // -tge- post action for single post
+    public function postAction()
+    {
+        // -tge- obtain parameter from URI
+        $postID = (int)$this->getEvent()->getRouteMatch()->getParam('postID');
+
+        // -tge- or we can use these commands
+        //$postID= $this->params('postID');
+        //$postID = $this->params()->fromRoute('postID');
+
+        return new ViewModel();
+    }
+
+    // -tge- post action for comments for post
+    public function commentsAction()
+    {
+        // -tge- obtain parameter from URI
+        $postID = (int)$this->getEvent()->getRouteMatch()->getParam('postID');
+
+        // -tge- or we can use these commands
+        //$postID= $this->params('postID');
+        //$postID = $this->params()->fromRoute('postID');
+
+        return new ViewModel();
+    }
+
+}
---
+++ module/TgeHelloWorld/config/module.config.php
@@ -0,0 +1,109 @@
+<?php
+/**
+* Zend Framework (http://framework.zend.com/)
+*
+* @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+* @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+* @license   http://framework.zend.com/license/new-bsd New BSD License
+*/
+
+return array(
+    'router' => array(
+        'routes' => array(
+            'home' => array(
+                'type' => 'Zend\Mvc\Router\Http\Literal',
+                'options' => array(
+                    'route'    => '/',
+                    'defaults' => array(
+                        'controller' => 'Application\Controller\Index',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+            // The following is a route to simplify getting started creating
+            // new controllers and actions without needing to create a new
+            // module. Simply drop new controllers in, and you can access them
+            // using the path /application/:controller/:action
+
+            // -tge- here is the key for our new module
+            'tgehelloworld' => array(
+                'type'    => 'Literal',
+                'options' => array(
+                    'route'    => '/tgehelloworld',
+                    'defaults' => array(
+                        '__NAMESPACE__' => 'TgeHelloWorld\Controller', // -tge- and namespace
+                        'controller'    => 'Index',
+                        'action'        => 'index',
+                    ),
+                ),
+                'may_terminate' => true,
+                'child_routes' => array(
+                    'default' => array(
+                        'type'    => 'Segment',
+                        'options' => array(
+                            'route'    => '/tgehelloworld/[:controller[/:action]]',  // -tge- we will use in future
+                            'constraints' => array(
+                                'controller' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                                'action'     => '[a-zA-Z][a-zA-Z0-9_-]*',
+                            ),
+                            'defaults' => array(
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        ),
+    ),
+    'service_manager' => array(
+        'abstract_factories' => array(
+            'Zend\Cache\Service\StorageCacheAbstractServiceFactory',
+            'Zend\Log\LoggerAbstractServiceFactory',
+        ),
+        'aliases' => array(
+            'translator' => 'MvcTranslator', // -tge- we can keep translator
+        ),
+    ),
+    'translator' => array(
+        'locale' => 'en_US',
+        'translation_file_patterns' => array(
+            array(
+                'type'     => 'gettext',
+                'base_dir' => __DIR__ . '/../language',
+                'pattern'  => '%s.mo',
+            ),
+        ),
+    ),
+    'controllers' => array(
+        'invokables' => array(
+            'TgeHelloWorld\Controller\Index' => 'TgeHelloWorld\Controller\IndexController'
+        ),
+    ),
+    'view_manager' => array(
+        'display_not_found_reason' => true,
+        'display_exceptions'       => true,
+        'doctype'                  => 'HTML5',
+        'not_found_template'       => 'error/404',
+        'exception_template'       => 'error/index',
+        'template_map' => array(
+        
+            // -tge- our custom view
+            'tge-hello-world/index/index' => __DIR__ . '/../view/tge-hello-world/index/index.phtml', // -tge- our new view for our module
+            
+            // -tge- these views we inherited from Application module then we dont need them now
+            
+            //'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',  // -tge- we will use generic layout in Application module
+            //'error/404'               => __DIR__ . '/../view/error/404.phtml',  // -tge- we dont need custom 404-like templates for our module
+            //'error/index'             => __DIR__ . '/../view/error/index.phtml',
+        ),
+        'template_path_stack' => array(
+            __DIR__ . '/../view',
+        ),
+    ),
+    // Placeholder for console routes
+    'console' => array(
+        'router' => array(
+            'routes' => array(
+            ),
+        ),
+    ),
+);
